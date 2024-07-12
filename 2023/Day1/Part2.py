9ef078194489ab7e3c9635498c30ebb229cbc1dc
Pistolmannen <erikgustafsson03@gmail.com>
2024-07-12 20:22:37 +0200

    fixed text file

---
+++ 2023/Day1/Part2.py
@@ -0,0 +1,33 @@
+
+with open("Day1-Input.txt") as file:
+    fullText = file.read()
+
+textSplit = fullText.split("\n");
+
+answer = 0;
+
+for text in textSplit:
+    print(text);
+
+    textSize = len(text);
+    pos = textSize - 1;
+
+    firstNumber = 0;
+    secondNumber = 0;
+
+    for x in text:
+        if x.isdigit():
+            firstNumber = x
+            break;
+
+    for x in text:
+        if text[pos].isdigit():
+            secondNumber = text[pos];
+            break;
+        pos = pos - 1;
+
+    fullNumber = firstNumber + secondNumber;
+    print(fullNumber);
+    answer += int(fullNumber);
+
+print(answer);
